021243cae078cd7c6e44346dc848cd856080ba07
kdXiaoyi <9355034+kdXiaoyi@user.noreply.gitee.com>
2022-01-16 10:30:13 +0800

    FIX

--- main.bat
+++ main.bat
@@ -20,7 +20,7 @@ echo.
 echo  Made by kdXiaoyi. 2022版权所有
 echo ===================================================================
 echo SysBit=x%SysBit%
-choice.exe /c 1234567890 /N /M 从中选择一项^>
+api\choice.exe /c p /N /M 从中选择一项^>
 if %ERRORLEVEL%==0 (
     rem 用户中断操作。
     echo NO TRUE CHOICE INTUT
@@ -29,6 +29,11 @@ if %ERRORLEVEL%==255 (
     rem CHOICE.exe发出错误状态码
     set ERRORCODE=CSBB/API:Choice.ErrorEffect
     goto Error
+)
+if %ERRORLEVEL%==1 (
+    rem 模拟崩溃
+    set errorcode=CSBB/main:Debug.BOOM
+    goto error
 )
 goto menu
 
@@ -44,32 +49,54 @@ if EXIST %windir%\SysWOW64\ (
 )
 echo SysBit=x%SysBit%
 rem 初始化API调用
-cd "%cd%\API"
 goto menu
 
 :Error
 rem 显示崩溃信息
-color FC
+color F4
 title Stopped by a error ^| ERRORCODE^>%Errorcode%
 rem 写出崩溃信息
 set time2=%time%
-md "%temp%\CSBB\Crash\"
-set CrashFile="%temp%\CSBB\Crash-s\%time2%\"
-echo Crash Report >>%CrashFile%
-echo ================================================= >>%CrashFile%
-echo  ^> Oh,no,this is the 115414th crash report! >>%CrashFile%
-echo. >>%CrashFile%
-echo Time = %time2% >>%CrashFile%
-echo Errorcode = %errorcode% >>%CrashFile%
-echo SysBit = x%SysBit% >>%CrashFile%
-echo SysVer =  >>%CrashFile%
-Ver >>%CrashFile%
-echo Lang = %LANG% >>%CrashFile%
-echo SysDrive = %SystemDrive% >>%CrashFile%
-echo PROCESSOR ARCHITECTURE = %PROCESSOR_ARCHITECTURE% >>%CrashFile%
-echo OS Center = %OS% >>%CrashFile%
-echo Command Spec = %ComSpec% >>%CrashFile%
-echo Temp File = %temp% >>%CrashFile%
-echo App Ver = %AppVer% >>%CrashFile%
-echo. >>%CrashFile%
-echo Sub.End () >>%CrashFile%
+set i=1
+md "%temp%CSBB\Crash\"
+set CrashFile="%temp%CSBB\Crash-s\%time2%\"
+echo Crash Report >>"%CrashFile%\CRASH_REPORT.log"
+echo ================================================= >>"%CrashFile%\CRASH_REPORT.log"
+set i=20
+echo  ^> Oh,no,this is the 115414th crash report! >>"%CrashFile%\CRASH_REPORT.log"
+echo. >>"%CrashFile%\CRASH_REPORT.log"
+echo Time = %time2% >>"%CrashFile%\CRASH_REPORT.log"
+echo Errorcode = %errorcode% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysBit = x%SysBit% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysVer =  >>"%CrashFile%\CRASH_REPORT.log"
+Ver >>"%CrashFile%\CRASH_REPORT.log"
+set i=50
+echo Lang = %LANG% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysDrive = %SystemDrive% >>"%CrashFile%\CRASH_REPORT.log"
+echo PROCESSOR ARCHITECTURE = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
+echo OS Center = %OS% >>"%CrashFile%\CRASH_REPORT.log"
+echo Command Spec = %ComSpec% >>"%CrashFile%\CRASH_REPORT.log"
+echo Temp File = %temp% >>"%CrashFile%\CRASH_REPORT.log"
+echo App Ver = %AppVer% >>"%CrashFile%\CRASH_REPORT.log"
+echo. >>"%CrashFile%\CRASH_REPORT.log"
+set i=98
+echo Sub.End () >>"%CrashFile%\CRASH_REPORT.log"
+set i=100
+rem 打印崩溃信息
+cls
+echo.
+echo    : (
+echo .
+echo     This program ran into a problem and needs to restart. We're just
+echo     collecting some error info, and then we'll show them for you.
+echo.
+echo      %i%% complete
+echo.
+echo            If you want to feed back, give me this info:
+echo            E-Mail Address : popo0713@foxmail.com
+echo            Stop code : %errorcode%
+echo.
+echo  Any key to open error info.
+echo %CrashFile%
+pause>nul
+%CrashFile%
