f501407d9d5d2bbd68cbbdb5716cd18ca82ac97a
kdXiaoyi <9355034+kdXiaoyi@user.noreply.gitee.com>
2022-01-18 17:18:45 +0800

    Fix

--- main.bat
+++ main.bat
@@ -6,7 +6,72 @@ goto HEAD
 @echo off
 cls
 title CSBB
-goto api_load
+goto CSBB/api.load
+
+:CSBB/api.load
+rem 检测系统位数
+set SysBit=UNKNOW
+if EXIST %windir%\SysWOW64\ (
+    rem x64
+    set SysBit=64
+) ELSE (
+    rem x86
+    set SysBit=86
+)
+echo SysBit=x%SysBit%
+rem 初始化API调用
+goto menu
+
+:Error
+rem 显示崩溃信息
+color F4
+title Stopped by a error ^| ERRORCODE^>%Errorcode%
+rem 写出崩溃信息
+set i=1
+set CrashFile="%temp%\CSBB\Crash-s\%Random%%Random%%Random%%Random%%Random%\"
+md %CrashFile%
+echo Crash Report >>"%CrashFile%\CRASH_REPORT.log"
+echo ================================================= >>"%CrashFile%\CRASH_REPORT.log"
+set i=20
+echo  ^> Oh,no,this is the 115414th crash report! >>"%CrashFile%\CRASH_REPORT.log"
+echo. >>"%CrashFile%\CRASH_REPORT.log"
+echo Time = %time2% >>"%CrashFile%\CRASH_REPORT.log"
+echo Errorcode = %errorcode% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysBit = x%SysBit% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysVer =  >>"%CrashFile%\CRASH_REPORT.log"
+Ver >>"%CrashFile%\CRASH_REPORT.log"
+set i=50
+echo Lang = %LANG% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysDrive = %SystemDrive% >>"%CrashFile%\CRASH_REPORT.log"
+echo PROCESSOR ARCHITECTURE = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
+echo OS Center = %OS% >>"%CrashFile%\CRASH_REPORT.log"
+echo Command Spec = %ComSpec% >>"%CrashFile%\CRASH_REPORT.log"
+echo Temp File = %temp% >>"%CrashFile%\CRASH_REPORT.log"
+echo App Ver = %AppVer% >>"%CrashFile%\CRASH_REPORT.log"
+echo CPU NAME = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
+echo CPU INFO = %PROCESSOR_ARCHITEW6432% >>"%CrashFile%\CRASH_REPORT.log"
+echo. >>"%CrashFile%\CRASH_REPORT.log"
+set i=98
+echo Sub.End () >>"%CrashFile%\CRASH_REPORT.log"
+set i=100
+rem 打印崩溃信息
+cls
+echo.
+echo    : (
+echo .
+echo     This program ran into a problem and needs to restart. We're just
+echo     collecting some error info, and then we'll show them for you.
+echo.
+echo      %i%% complete
+echo.
+echo            If you want to feed back, give me this info:
+echo            E-Mail Address : popo0713@foxmail.com
+echo            Stop code : %errorcode%
+echo.
+echo  Any key to open error info.
+pause>nul
+api\OpenURL.exe -e -u %CrashFile%
+exit
 
 :menu
 cls
@@ -39,7 +104,7 @@ if %ERRORLEVEL%==1 (
 )
 if %ERRORLEVEL%==2 goto sys_show/menu
 if %ERRORLEVEL%==3 goto user/menu
-if %ERRORLEVEL%==4 goto user/menu
+if %ERRORLEVEL%==4 goto CSBB/about
 if %ERRORLEVEL%==5 goto menu.exit
 if %ERRORLEVEL%==6 goto menu.exit
 goto menu
@@ -114,67 +179,3 @@ echo SysBit=x%SysBit%
 api\choice.exe /c 0 /N /M 从中选择一项^>
 if %ERRORLEVEL%==1 goto menu
 goto user_menu
-
-:api_load
-rem 检测系统位数
-set SysBit=UNKNOW
-if EXIST %windir%\SysWOW64\ (
-    rem x64
-    set SysBit=64
-) ELSE (
-    rem x86
-    set SysBit=86
-)
-echo SysBit=x%SysBit%
-rem 初始化API调用
-goto menu
-
-:Error
-rem 显示崩溃信息
-color F4
-title Stopped by a error ^| ERRORCODE^>%Errorcode%
-rem 写出崩溃信息
-set i=1
-set CrashFile="%temp%\CSBB\Crash-s\%Random%%Random%%Random%%Random%%Random%\"
-md %CrashFile%
-echo Crash Report >>"%CrashFile%\CRASH_REPORT.log"
-echo ================================================= >>"%CrashFile%\CRASH_REPORT.log"
-set i=20
-echo  ^> Oh,no,this is the 115414th crash report! >>"%CrashFile%\CRASH_REPORT.log"
-echo. >>"%CrashFile%\CRASH_REPORT.log"
-echo Time = %time2% >>"%CrashFile%\CRASH_REPORT.log"
-echo Errorcode = %errorcode% >>"%CrashFile%\CRASH_REPORT.log"
-echo SysBit = x%SysBit% >>"%CrashFile%\CRASH_REPORT.log"
-echo SysVer =  >>"%CrashFile%\CRASH_REPORT.log"
-Ver >>"%CrashFile%\CRASH_REPORT.log"
-set i=50
-echo Lang = %LANG% >>"%CrashFile%\CRASH_REPORT.log"
-echo SysDrive = %SystemDrive% >>"%CrashFile%\CRASH_REPORT.log"
-echo PROCESSOR ARCHITECTURE = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
-echo OS Center = %OS% >>"%CrashFile%\CRASH_REPORT.log"
-echo Command Spec = %ComSpec% >>"%CrashFile%\CRASH_REPORT.log"
-echo Temp File = %temp% >>"%CrashFile%\CRASH_REPORT.log"
-echo App Ver = %AppVer% >>"%CrashFile%\CRASH_REPORT.log"
-echo CPU NAME = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
-echo CPU INFO = %PROCESSOR_ARCHITEW6432% >>"%CrashFile%\CRASH_REPORT.log"
-echo. >>"%CrashFile%\CRASH_REPORT.log"
-set i=98
-echo Sub.End () >>"%CrashFile%\CRASH_REPORT.log"
-set i=100
-rem 打印崩溃信息
-cls
-echo.
-echo    : (
-echo .
-echo     This program ran into a problem and needs to restart. We're just
-echo     collecting some error info, and then we'll show them for you.
-echo.
-echo      %i%% complete
-echo.
-echo            If you want to feed back, give me this info:
-echo            E-Mail Address : popo0713@foxmail.com
-echo            Stop code : %errorcode%
-echo.
-echo  Any key to open error info.
-pause>nul
-api\OpenURL.exe -e -u %CrashFile%
