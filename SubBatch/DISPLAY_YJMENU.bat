187fe4c0b07723ec385847451fbd78819e873af0
kdXiaoyi <9355034+kdXiaoyi@user.noreply.gitee.com>
2022-01-21 12:53:03 +0800

    fix bug -> Error files won't show turly  <!>The end of %temp% must isn't "\" or "/"

--- SubBatch/DISPLAY_YJMENU.bat
+++ SubBatch/DISPLAY_YJMENU.bat
@@ -8,56 +8,7 @@ if "%sisbit%"=="86" (
         rem ≠x86          ≠x64
         rem 不合要求
         rem 显示崩溃信息
-        set errorcode=CSBB/DISPLAY_MENU:SysBit.false_number
-        color F4
-        title Stopped by a error ^| ERRORCODE^>%Errorcode%
-        rem 写出崩溃信息
-        set i=1
-        set CrashFile="%temp%\CSBB\Crash-s\%Random%%Random%%Random%%Random%%Random%\"
-        md %CrashFile%
-        echo Crash Report >>"%CrashFile%\CRASH_REPORT.log"
-        echo ================================================= >>"%CrashFile%\CRASH_REPORT.log"
-        set i=20
-        echo  ^> Oh,no,this is the 115414th crash report! >>"%CrashFile%\CRASH_REPORT.log"
-        echo. >>"%CrashFile%\CRASH_REPORT.log"
-        echo Time = %time2% >>"%CrashFile%\CRASH_REPORT.log"
-        echo Errorcode = %errorcode% >>"%CrashFile%\CRASH_REPORT.log"
-        echo SysBit = x%SysBit% >>"%CrashFile%\CRASH_REPORT.log"
-        echo SysVer =  >>"%CrashFile%\CRASH_REPORT.log"
-        Ver >>"%CrashFile%\CRASH_REPORT.log"
-        set i=50
-        echo Lang = %LANG% >>"%CrashFile%\CRASH_REPORT.log"
-        echo SysDrive = %SystemDrive% >>"%CrashFile%\CRASH_REPORT.log"
-        echo PROCESSOR ARCHITECTURE = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
-        echo OS Center = %OS% >>"%CrashFile%\CRASH_REPORT.log"
-        echo Command Spec = %ComSpec% >>"%CrashFile%\CRASH_REPORT.log"
-        echo Temp File = %temp% >>"%CrashFile%\CRASH_REPORT.log"
-        echo App Ver = %AppVer% >>"%CrashFile%\CRASH_REPORT.log"
-        echo CPU NAME = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
-        echo CPU INFO = %PROCESSOR_ARCHITEW6432% >>"%CrashFile%\CRASH_REPORT.log"
-        echo. >>"%CrashFile%\CRASH_REPORT.log"
-        set i=98
-        echo Sub.End () >>"%CrashFile%\CRASH_REPORT.log"
-        set i=100
-        rem 打印崩溃信息
-        cls
-        echo.
-        echo    : (
-        echo.
-        echo     This program ran into a problem and needs to restart. We're just
-        echo     collecting some error info, and then we'll show them for you.
-        echo.
-        echo      %i%% complete
-        echo.
-        echo            If you want to feed back, give me this info:
-        echo            E-Mail Address : popo0713@foxmail.com
-        echo            Stop code : %errorcode%
-        echo.
-        echo  Any key to open error info.
-        echo   ERROR_INFO_FILE=%CrashFile%
-        pause>nul
-        api\OpenURL.exe -u file:///%CrashFile%
-        exit
+        goto error
     )
     rem x64
     rem 合要求
@@ -102,3 +53,55 @@ if "%password%"=="%input%" (
     pause>nul
 )
 call main.bat
+
+:error
+set errorcode=CSBB/DISPLAY_MENU:SysBit.false_number
+color F4
+title Stopped by a error ^| ERRORCODE^>%Errorcode%
+rem 写出崩溃信息
+set i=1
+set CrashFile=%temp%\CSBB\Crash-s\%random%%RANDOM%%Random%
+md %CrashFile%
+echo Crash Report >>"%CrashFile%\CRASH_REPORT.log"
+echo ================================================= >>"%CrashFile%\CRASH_REPORT.log"
+set i=20
+echo  ^> Oh,no,this is the 115414th crash report! >>"%CrashFile%\CRASH_REPORT.log"
+echo. >>"%CrashFile%\CRASH_REPORT.log"
+echo Time = %time2% >>"%CrashFile%\CRASH_REPORT.log"
+echo Errorcode = %errorcode% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysBit = x%SysBit% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysVer =  >>"%CrashFile%\CRASH_REPORT.log"
+Ver >>"%CrashFile%\CRASH_REPORT.log"
+set i=50
+echo Lang = %LANG% >>"%CrashFile%\CRASH_REPORT.log"
+echo SysDrive = %SystemDrive% >>"%CrashFile%\CRASH_REPORT.log"
+echo PROCESSOR ARCHITECTURE = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
+echo OS Center = %OS% >>"%CrashFile%\CRASH_REPORT.log"
+echo Command Spec = %ComSpec% >>"%CrashFile%\CRASH_REPORT.log"
+echo Temp File = %temp% >>"%CrashFile%\CRASH_REPORT.log"
+echo App Ver = %AppVer% >>"%CrashFile%\CRASH_REPORT.log"
+echo CPU NAME = %PROCESSOR_ARCHITECTURE% >>"%CrashFile%\CRASH_REPORT.log"
+echo CPU INFO = %PROCESSOR_ARCHITEW6432% >>"%CrashFile%\CRASH_REPORT.log"
+echo. >>"%CrashFile%\CRASH_REPORT.log"
+set i=98
+echo Sub.End () >>"%CrashFile%\CRASH_REPORT.log"
+set i=100
+rem 打印崩溃信息
+cls
+echo.
+echo    : (
+echo.
+echo     This program ran into a problem and needs to restart. We're just
+echo     collecting some error info, and then we'll show them for you.
+echo.
+echo      %i%% complete
+echo.
+echo    If you want to feed back (Up a Issues) , give me these info:
+echo    E-Mail Address : popo0713@foxmail.com
+echo    Stop code : %errorcode%
+echo.
+echo  Any key to open error info.
+echo   ERROR_INFO_FILE=%CrashFile%
+pause>nul
+api\OpenURL.exe -u file:///%CrashFile%
+exit
